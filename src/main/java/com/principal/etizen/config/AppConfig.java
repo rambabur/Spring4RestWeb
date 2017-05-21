93fe5c4a0c9803516ee631731205362e58fda2ed
Ramababu Ravuri <ramababur@gmail.com>
2017-05-21 02:04:46 -0500

    Initial commit

---
+++ src/main/java/com/principal/etizen/config/AppConfig.java
@@ -0,0 +1,12 @@
+package com.principal.etizen.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.principal.etizen")
+public class AppConfig {
+
+}
